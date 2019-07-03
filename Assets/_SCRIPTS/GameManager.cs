c13388f6e26d6de05408510f7d00722e886d01e4
priscillavasconcelos <priscillavasconcelos.vox@gmail.com>
2019-07-02 22:41:13 -0300

    complete game
    complete game
    unity version 2018.4

---
+++ Assets/_SCRIPTS/GameManager.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject food;
+    public Vector3 center;
+    public Vector3 spawnAreaSize;
+
+    public GameObject outline;
+    public float colThickness = 4f;
+    public float zPosition = 0f;
+    private Vector2 screenSize;
+
+    public static GameManager gm;
+
+    private void Awake()
+    {
+        if (gm == null)
+        {
+            gm = this;
+        }
+        //else if (gm != this)
+        //{
+        //    Destroy(gameObject);
+        //}
+        //DontDestroyOnLoad(gameObject);
+
+        SettingBorders();
+
+        spawnAreaSize = new Vector3((screenSize.x * 2 - (colThickness*2)) - 0.5f, (screenSize.y * 2 - (colThickness * 2)) - 0.5f, colThickness);
+    }
+
+    void Start()
+    {
+        SpawnFood();
+    }
+
+    public void SpawnFood()
+    {
+        float posX = Mathf.Round(Random.Range(-spawnAreaSize.x / 2, spawnAreaSize.x / 2));
+        float posY = Mathf.Round(Random.Range(-spawnAreaSize.y / 2, spawnAreaSize.y / 2));
+        Vector3 pos = center + new Vector3(posX, posY, 1f);
+        Instantiate(food, pos, Quaternion.identity);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawCube(center, spawnAreaSize);
+    }
+
+    void SettingBorders()
+    {
+        Vector3 cameraPos = Camera.main.transform.position;
+
+        screenSize.x = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0))) * 0.5f;
+        screenSize.y = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height))) * 0.5f;
+
+        Dictionary<string, Transform> colliders = new Dictionary<string, Transform>();
+
+        colliders.Add("Top", Instantiate(outline).transform);
+        colliders.Add("Bottom", Instantiate(outline).transform);
+        colliders.Add("Right", Instantiate(outline).transform);
+        colliders.Add("Left", Instantiate(outline).transform);
+
+        foreach (KeyValuePair<string, Transform> valPair in colliders)
+        {
+            valPair.Value.parent = transform;
+
+            if (valPair.Key == "Left" || valPair.Key == "Right")
+            {
+                valPair.Value.localScale = new Vector3(colThickness, screenSize.y * 2, colThickness);
+            }
+            else
+            {
+                valPair.Value.localScale = new Vector3(screenSize.x * 2, colThickness, colThickness);
+            }
+
+        }
+
+        colliders["Right"].position = new Vector3(cameraPos.x + screenSize.x - (colliders["Right"].localScale.x * 0.5f), cameraPos.y, zPosition);
+        colliders["Left"].position = new Vector3(cameraPos.x - screenSize.x + (colliders["Left"].localScale.x * 0.5f), cameraPos.y, zPosition);
+        colliders["Top"].position = new Vector3(cameraPos.x, cameraPos.y + screenSize.y - (colliders["Top"].localScale.y * 0.5f), zPosition);
+        colliders["Bottom"].position = new Vector3(cameraPos.x, cameraPos.y - screenSize.y + (colliders["Bottom"].localScale.y * 0.5f), zPosition);
+    }
+}
---
+++ Assets/_SCRIPTS/SnakeController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+
+public class SnakeController : MonoBehaviour
+{
+    public float speed;
+    public GameObject tailPrefab;
+    Vector2 dir = Vector2.right;
+
+    float timeToMoveCount, currentTimeToMove, initialTimeToMove = 0.3f;
+
+    List<Transform> tail = new List<Transform>();
+
+    bool ate;
+    void Start()
+    {
+        currentTimeToMove = initialTimeToMove;
+        timeToMoveCount = initialTimeToMove;
+        //InvokeRepeating("Movement", 0.3f, 0.3f);
+    }
+
+    void Update()
+    {
+        timeToMoveCount -= Time.deltaTime;
+        if (timeToMoveCount < 0)
+        {
+            Movement();
+            timeToMoveCount = currentTimeToMove;
+        }
+        
+        float dirX = Input.GetAxis("Horizontal");
+        float dirY = Input.GetAxis("Vertical");
+
+        if (dirX > 0)
+        {
+            dir = Vector2.right;
+        }
+        else if (dirX < 0)
+        {
+            dir = -Vector2.right;
+        }
+        else if (dirY > 0)
+        {
+            dir = Vector2.up;
+        }
+        else if (dirY < 0)
+        {
+            dir = -Vector2.up;
+        }
+    }
+
+    void Movement()
+    {
+        Vector2 currentPosition = transform.position;
+        transform.Translate(dir);
+
+        if (ate)
+        {
+            GameObject newTail = (GameObject)Instantiate(tailPrefab, currentPosition, Quaternion.identity);
+            tail.Insert(0, newTail.transform);
+            ate = false;
+            GameManager.gm.SpawnFood();
+            currentTimeToMove -= 0.01f;
+        }
+
+        if (tail.Count > 0)
+        {
+            tail.Last().position = currentPosition;
+
+            tail.Insert(0, tail.Last());
+            tail.RemoveAt(tail.Count - 1);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            ate = true;
+
+            Destroy(collision.gameObject);
+        }
+        else
+        {
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
+    }
+}
